648ba29b00122c40a24ce0a866f3594b0b4325f0
Low Kian Seong <kianseong@gmail.com>
2014-12-22 15:46:18 +0800

    Cleaned up example for write_to_file

--- src/write_to_file.rs
+++ src/write_to_file.rs
@@ -1,6 +1,27 @@
 fn main() {
 	use std::io::File;
 	let path = Path::new("foo.txt");
-	let mut file = File::create(&path);
-	file.write(b"Merry Christmas hoho!");
+        let display = path.display();
+
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("Coult not create bro! {} {}", display, why.desc),
+            Ok(file) => file,
+        };
+
+	match file.write(b"Merry Christmas hoho!") {
+            Err(why) => panic!("Coult not write to file bro! {} {}", display, why.desc),
+            Ok(_) => println!("Successfully written to file"),
+        };
+        
+
+        let mut file_to_read = match File::open(&Path::new("foo.txt")) {
+            Err(why) => panic!("File not found bro! {} {}", display, why.desc),
+            Ok(file) => file,
+        };
+
+
+        match file_to_read.read_to_string() {
+            Err(why) => panic!("File can't be read bro! {} {}", display, why.desc),
+            Ok(string) => println!("{} contains this {}", display, string),
+        }
 }
