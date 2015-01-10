abb5acbf76ebb8e2371a3adf139ae9df85cb3954
lowks <kianseong@gmail.com>
2015-01-10 20:10:11 +0800

    Enhancing the command line example

--- src/command_line_arguments.rs
+++ src/command_line_arguments.rs
@@ -2,5 +2,9 @@ use std::os;
 
 fn main() {
 	let args = os::args();
-	println!("The argument is {}", args[1]);
+	let first_args = &args[1].to_string();
+	match &**first_args {
+		"test" => println!("Good!"),
+		_ => println!("No test ?!"),
+	}
 }
