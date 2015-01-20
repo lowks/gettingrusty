dcb3283bb3e23e81834890c66b8bc6deffba2886
Low Kian Seong <kianseong@gmail.com>
2015-01-20 17:04:15 +0800

    Improving the listdir example, pattern matching FTW!

--- src/listdir.rs
+++ src/listdir.rs
@@ -33,6 +33,21 @@ pub mod list {
             false => "Not file!".to_string(),
         }
     }
+
+    pub fn is_it_file_or_dir(input_file: &str) -> String {
+        let path3 = Path::new(input_file);  
+        match path3.is_file() {
+            
+            true => "File!".to_string(),
+            false => match path3.is_dir() {
+
+                true => "Dir!".to_string(),
+                _ => "Dont care!".to_string(),
+
+            }
+        }
+    }
+    
 }
 
 
@@ -41,4 +56,5 @@ pub mod list {
 fn test_path(){
     assert_eq!(list::does_it_exist("/tmp"), "Exists!");
     assert_eq!(list::is_it_file("/tmp"), "Not file!");
+    assert_eq!(list::is_it_file_or_dir("/tmp"), "Dir!");
 }
