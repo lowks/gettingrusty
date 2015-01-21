6e5c72d1b9ba5bd305a5c34b4c759a615172b926
Low Kian Seong <kianseong@gmail.com>
2015-01-21 09:53:43 +0800

    adding test for pattern_match

--- src/pattern_match.rs
+++ src/pattern_match.rs
@@ -1,15 +1,22 @@
 pub mod pattern_m {
-    pub fn main() {
+    pub fn main() -> int {
         let x = 5;
 
         match x {
-            5 => println!("Number is 5"),
-            _ => println!("Some other number"),
+            5 => 5, 
+            _ => 0,
         }
 
-        match x {
-            a @ 1...5 => println!("{} is within 1 to 5", a),
-            _ => println!("Not within 1 to 5"),
-        }
+        // match x {               
+        //     a @ 1...5 => println!("{} is within 1 to 5", a),
+        //     _ => println!("Not within 1 to 5"),
+        //}
     }
 }
+
+#[test]
+
+fn test_pattern_m() {
+    assert_eq!(pattern_m::main(), 5)
+}
+
