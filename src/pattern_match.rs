45148d79d0a810a5b19466558cf701245abdf214
Low Kian Seong <kianseong@gmail.com>
2015-03-11 12:16:45 +0800

    Clean up code

--- src/pattern_match.rs
+++ src/pattern_match.rs
@@ -1,16 +1,10 @@
 pub mod pattern_m {
     pub fn main() -> int {
         let x = 5;
-
         match x {
             5 => 5, 
             _ => 0,
         }
-
-        // match x {               
-        //     a @ 1...5 => println!("{} is within 1 to 5", a),
-        //     _ => println!("Not within 1 to 5"),
-        //}
     }
 }
 
