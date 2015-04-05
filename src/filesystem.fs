95a33dba53f6f461df106efa0f552127a437c634
lowks <kianseong@gmail.com>
2015-04-05 12:15:41 +0800

    Fixing borked stuff again

--- src/filesystem.fs
+++ src/filesystem.fs
@@ -1,9 +1,11 @@
-use std::io;
-use std::io::fs;
+#![feature(old_io, old_path, os, old_fs)]
+use std::old_path::{Path};
+use std::old_io::{fs};
+use std::old_io;
 
 fn main() {
 	let p = Path::new("tmp");
-	fs::mkdir(&p, io::UserRWX);
+	fs::mkdir(&p, old_io::USER_RWX);
 	fs::rmdir(&p);
 }
 
