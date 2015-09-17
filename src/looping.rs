9b124f14f25a51299373fc4823583ccc4493ace6
lowks <kianseong@gmail.com>
2015-09-18 01:34:18 +0800

    fixing looping.rs

--- src/looping.rs
+++ src/looping.rs
@@ -1,18 +1,19 @@
+pub mod looping {
 fn print_stuff() {
-for x in (0..10) {
-    println!( "{}", x);
-}
+	for x in (0..10) {
+	    println!( "{}", x);
+	}
 
-println!("Iter function loop");
+	println!("Iter function loop");
 
-let values = vec![1, 2, 3, 4];
-for &x in values.iter() {
-    println!("{}", x);
+	let values = vec![1, 2, 3, 4];
+	for &x in values.iter() {
+	    println!("{}", x);
+	}
 }
-}
-
-fn main() {
-   print_stuff();
+	fn main() {
+	   print_stuff();
+	}
 }
 
 
