cb9e9a7baee9723c864ffd1bf9c3e7a5e65cb32d
Low Kian Seong <kianseong.low@onapp.com>
2015-09-18 19:59:49 +0800

    Changing the way function_goodbye_world works

--- src/function_goodbye_world.rs
+++ src/function_goodbye_world.rs
@@ -1,7 +1,13 @@
 pub mod goodbye_world {
     pub mod goodbye_world {
-        pub fn print_good_bye_world() {
-	    println!("Good Bye, cruel world inside of a function !");
+        pub fn print_good_bye_world() -> &'static str {
+	         "good bye world"
         }
     }
+}
+
+#[test]
+
+fn test_for_goodbye_world() {
+        assert_eq!(goodbye_world::goodbye_world::print_good_bye_world(), "good bye world");
 }
