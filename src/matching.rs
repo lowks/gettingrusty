07c85ae6d706a1c32f45221ebc19836ca0c6ac09
Low Kian Seong <kianseong.low@onapp.com>
2016-01-04 14:30:17 +0800

    Correct the indentation for matching.rs

--- src/matching.rs
+++ src/matching.rs
@@ -1,11 +1,12 @@
 pub mod matches {
-pub fn main() {
-let x = 1;
 
-match x {
-    1 => println!("I am 1"),
-    2 => println!("You will never see me because no match!"),
-    _ => println!("Alamak!"),
-}
-}
+    pub fn main() {
+
+        let x = 1;
+        match x {
+            1 => println!("I am 1"),
+            2 => println!("You will never see me because no match!"),
+            _ => println!("Alamak!"),
+        }
+    }
 }
