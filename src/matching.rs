53f4531322705017543ed2ab4e9f01a53d40d1c6
Low Kian Seong <kianseong.low@onapp.com>
2016-01-05 18:07:17 +0800

    Adding array example

--- src/matching.rs
+++ src/matching.rs
@@ -1,11 +1,17 @@
 pub mod matches {
-pub fn main() {
-let x = 1;
+    pub fn main() {
 
-match x {
-    1 => println!("I am 1"),
-    2 => println!("You will never see me because no match!"),
-    _ => println!("Alamak!"),
-}
-}
+        let x = 1;
+        match x {
+            1 => println!("I am 1"),
+            2 => println!("You will never see me because no match!"),
+            _ => println!("Alamak!"),
+        }
+
+        let y = 'a';
+        match y {
+            x => println!("ho ho ho"),
+        }
+        println!("X is ... {}", x)
+    }
 }
