4b98a16d61771e58c640c136f6fcb6e1d62a1fb7
lowks <kianseong@gmail.com>
2016-01-11 23:12:04 +0800

    Enhanced looping.rs with more examples

--- src/looping.rs
+++ src/looping.rs
@@ -1,5 +1,24 @@
 pub mod looping {
 fn print_stuff() {
+
+    for x in 1..15 {
+        if x % 3 == 0 {
+            println!("Number: {}", x);
+        }
+    }
+
+    for (i, j) in (5..10).enumerate() {
+
+        println!("i = {} and j = {}", i, j)
+
+    }
+
+    for x in 0..10 {
+        if x % 3 == 0 { continue; }
+
+        println!("Example using continue {}", x)
+    }
+
 	for x in 0..10 {
 	    println!( "{}", x);
 	}
