d85a625e4f8fb4deba64467de8355ad294118d6b
Low Kian Seong <kianseong.low@onapp.com>
2016-03-04 18:00:38 +0800

    Enhance example

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,6 +1,5 @@
 pub mod conditional {
 	pub fn tester(a: i32) -> i32 {
-	    // let a = 10;
 	    if a < 100 || a > 9 {
 		println!("helloworld from conditionals");
 	    }
@@ -11,7 +10,10 @@ pub mod conditional {
 
 	}
         pub fn main() {
-	   	assert_eq!(tester(10), 0);
+            let x = 5;
+            let y = if x == 5 {10} else {15};
+            println!("y is {}", y);
+	    assert_eq!(tester(10), 0);
 	}
 }
 
